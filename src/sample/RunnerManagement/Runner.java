ffb130ba1fff3a43a3bbd6fe9eb433320faf9bf5
A. Berkay Bal <36015894+akinizer@users.noreply.github.com>
2021-03-05 12:43:31 +0300

    Projectile and Runner: Code beautification

--- src/sample/RunnerManagement/Runner.java
+++ src/sample/RunnerManagement/Runner.java
@@ -15,6 +15,7 @@ import java.util.Timer;
 
 public class Runner extends Label {
 
+    //Components
     private TabPane maintab;
     private Tab towntab;
     StackPane stackPane;
@@ -23,12 +24,14 @@ public class Runner extends Label {
     private Button actionButton, speedButton;
     private ImageView iv;
 
+    //Attributes
     private int speed;
     private int distancecounter;
-    private double firerate, cooldown=2;
+    private double firerate, cooldown;
 
-
+    //Constructor
     public Runner(TabPane maintab, double width, double height, Button actionButton, Button speedButton, Tab towntab, StackPane stackPane){
+        //component settings
         this.maintab=maintab;
         this.width=width/2 -5;
         this.height=height/2 -5;
@@ -37,16 +40,19 @@ public class Runner extends Label {
         this.towntab=towntab;
         this.stackPane=stackPane;
 
+        //attribute initial values
         speed=1;
         distancecounter=0;
         firerate=1;
         cooldown=0;
 
+        //settings initialization
         setPresets();
         setMouseListener();
         setKeyListener();
     }
 
+    //settings
     private void setPresets(){
         setCarView();
         setGraphic(iv);
@@ -58,6 +64,8 @@ public class Runner extends Label {
         setTooltip();
         setVisible(false);
     }
+
+    //mouse events: start button, reset button, tooltip
     private void setMouseListener(){
         actionButton.setOnMouseReleased(mouseEvent -> {
             setTranslateX(width);
@@ -80,12 +88,15 @@ public class Runner extends Label {
         });
     }
 
+    //key events: movement, firing projectiles
     private int direction=0;
     private void setKeyListener(){
         maintab.setOnKeyPressed(keyEvent -> {
             if(towntab.isSelected()) {
                 //TODO: this setting disables cross movement,
                 // to ENABLE cross movement set all cases if and assign cross movement images with additional if cases
+
+                //movement and movement speed, direction, distance
                 switch (keyEvent.getText().toLowerCase()) {
                     case "d":
                         iv.setRotate(90);
@@ -116,44 +127,57 @@ public class Runner extends Label {
                         System.out.println("Key pressed: " + keyEvent.getText().toLowerCase());
                 }
 
-                long elapsed=(System.currentTimeMillis() - starttime)/1000;
-                if(starttime!=-1 && elapsed < cooldown) return;
+                //cooldown validation
+                if(isCooldownActive()) return;
 
+                //projectile generation and firing
                 if(keyEvent.getText().toLowerCase().equals("f")){
+                    //projectile
                     Projectile projectile = new Projectile(getTranslateX()+getWidth()/2.5,getTranslateY()+getHeight()/2.5);
                     stackPane.getChildren().add(projectile);
                     projectile.fire(direction,"missile");
+
+                    //get firerate and cooldown values from the projectile
                     firerate=projectile.getSpeed();
                     cooldown=1/firerate;
+
+                    //cooldown on fire
                     initiateCooldown();
                 }
             }
+            //tooltip on Runner to report stat values
             setTooltip();
         });
     }
 
+    //Tooltip Message
     private void setTooltip(){
         setTooltip(new Tooltip("Position: ("+getTranslateX()+":"+getTranslateY()+")"
                                 +"\nDistance: "+distancecounter+ " px"
                                 +"\nSpeed: "+speed
                                 +"\nFirerate: "+firerate
-                ));
+        ));
     }
+    //Runner View Interface
     private void setCarView(){
         iv=new ImageView(new Image(getClass().getResource("/sample/Resources/car-up.png").toExternalForm()));
         iv.setFitWidth(25);
         iv.setPreserveRatio(true);
     }
 
+    //Runner Movement Speed Update
     public void setSpeed(int speed){
         this.speed=speed;
     }
 
-
-    ///cooldown
+    //Cooldown Initializer and Validator
     private long starttime=-1;
     private void initiateCooldown(){
         starttime=System.currentTimeMillis();
+    }
+    private boolean isCooldownActive(){
+        long elapsed=(System.currentTimeMillis() - starttime)/1000;
+        return starttime!=-1 && elapsed < cooldown;
     }
 
 }
